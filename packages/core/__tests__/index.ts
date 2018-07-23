77062f807f96f09537d4796db0f832fe3db50705
Mitchell Valine <mitchellvaline@gmail.com>
2018-07-22 18:35:52 -0700

    Partial/Pipe/Compose
    Basic implementations of partial/pipe/compose function utilities.
    
    Partial is useful for applying less than the total number of expected
    arguments returning a new function that accepts the rest. This is a core
    util for composition without auto currying everything.
    
    Pipe/Compose are standard function composition tools to thread values
    through many functions. Both expect all functions in the sequence to be
    unary where some allow the first applied to be multi-arity and all
    subsequent to be unary.

--- packages/core/__tests__/index.ts
+++ packages/core/__tests__/index.ts
@@ -1,10 +1,32 @@
 /* eslint-env jest */
-import {add} from "../src"
+import {compose, partial, pipe} from "../src"
 
+const add = (x, y) => x + y
 describe("Kompose Core", () => {
-    describe("add", () => {
-        it("passes", () => {
-            expect(add(1, 1)).toEqual(2)
+    describe("partial", () => {
+        it("accepts an x arity function and an array of n args and returns a function with first n arguments applied with an x - n arity", () => {
+            const subject = partial(add, [1])
+            expect(subject(1)).toEqual(2)
+        })
+    })
+
+    describe("pipe", () => {
+        it("accepts an array of functions and a value and threads value through functions in order", () => {
+            const subject = pipe([
+                x => x + 1,
+                x => x * x
+            ], 2)
+            expect(subject).toEqual(9)
+        })
+    })
+
+    describe("compose", () => {
+        it("accepts an array of functions and a value and threads value through functions in reverse order", () => {
+            const subject = compose([
+                x => x + 1,
+                x => x *x
+            ], 2)
+            expect(subject).toEqual(5)
         })
     })
 })
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,5 @@
-export const add = (x: number, y: number): number => x + y
+export const partial = (fn, args) => (...argsRest) => fn(...args, ...argsRest)
+
+export const pipe = (fns, val) => fns.reduce((accum, fn) => fn(accum), val)
+
+export const compose = (fns, val) => fns.reduceRight((accum, fn) => fn(accum), val)
