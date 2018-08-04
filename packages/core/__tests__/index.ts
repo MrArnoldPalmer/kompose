f049af9f03a941afb1efacf805500ba72f43b6a4
Mitchell Valine <mitchellvaline@gmail.com>
2018-08-04 12:06:42 -0700

    Types for core functions
    Typescript 3.0 update has better support for rest arguments typing with
    tuples. This is theory should limit the need for overloading of function
    signatures in the way that the tTpipe interface is defined but I have
    yet to figure out the correct signature without the overloads.

--- packages/core/__tests__/index.ts
+++ packages/core/__tests__/index.ts
@@ -1,20 +1,19 @@
-/* eslint-env jest */
 import {compose, partial, pipe} from "../src"
 
 const add = (x, y) => x + y
 describe("Kompose Core", () => {
     describe("partial", () => {
         it("accepts an x arity function and an array of n args and returns a function with first n arguments applied with an x - n arity", () => {
-            const subject = partial(add, [1])
+            const subject: (number) => number = partial(add, [1])
             expect(subject(1)).toEqual(2)
         })
     })
 
     describe("pipe", () => {
         it("accepts an array of functions and a value and threads value through functions in order", () => {
-            const subject = pipe([
-                x => x + 1,
-                x => x * x
+            const subject: number = pipe([
+                (x: number): number => x + 1,
+                (x: number): number => x * x
             ], 2)
             expect(subject).toEqual(9)
         })
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,22 @@
-export const partial = (fn, args) => (...argsRest) => fn(...args, ...argsRest)
+/*
+ * Util Types
+ */
+type tUnary<A, R> = (A) => R
+type tBinary<A1, A2, R> = (A1, A2) => R
+type tVariant<A extends any[], R> = (...args: A) => R
 
-export const pipe = (fns, val) => fns.reduce((accum, fn) => fn(accum), val)
+type tPartial = <A extends any[], R>(fn: tVariant<A, R>, args: any[]) => (...rest: any[]) => R
+export const partial: tPartial = (fn, args) => (...argsRest) => fn(...args, ...argsRest)
+
+type tPipe = {
+    <V, R1>(fns: [tUnary<V, R1>], val: V): R1
+    <V, R1, R2>(fns: [(arg: V) => R1, (arg: R1) => R2], val: V): R2
+    <V, R1, R2, R3>(fns: [(arg: V) => R1, (arg: R1) => R2, (arg: R2) => R3], val: V): R3;
+    <V, R1, R2, R3, R4>(fns: [(arg: V) => R1, (arg: R1) => R2, (arg: R2) => R3, (arg: R3) => R4], val: V): R4;
+    <V, R1, R2, R3, R4, R5>(fns: [(arg: V) => R1, (arg: R1) => R2, (arg: R2) => R3, (arg: R3) => R4, (arg: R4) => R5], val: V): R5;
+}
+// type tPipe = <V, R>(fns: Array<tUnary<V, R>>, val: V) => R
+export const pipe: tPipe = (fns, val) => fns.reduce((accum, fn) => fn(accum), val)
+const x: number = pipe([(y: number): number => y + 1, (y: number): number => y + 1, (y: number): number => y + 1], 1);
 
 export const compose = (fns, val) => fns.reduceRight((accum, fn) => fn(accum), val)
