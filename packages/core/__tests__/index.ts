e63f6ac87eb1bd1e963808654e5cff17433013c2
Mitchell Valine <mitchellvaline@gmail.com>
2018-08-15 22:22:02 -0700

    More Functions and Formatting

--- packages/core/__tests__/index.ts
+++ packages/core/__tests__/index.ts
@@ -1,31 +1,162 @@
-import {compose, partial, pipe} from "../src"
+import * as K from "../src/index"
 
-const add = (x, y) => x + y
-describe("Kompose Core", () => {
-    describe("partial", () => {
-        it("accepts an x arity function and an array of n args and returns a function with first n arguments applied with an x - n arity", () => {
-            const subject: (number) => number = partial(add, [1])
-            expect(subject(1)).toEqual(2)
-        })
+describe("Utils", () => {
+  describe("apply", () => {
+    it("applies an array of arguments to a function", () => {
+      const subject = K.apply((x, y, z) => [x, y, z], [1, 2, 3])
+      expect(subject).toEqual([1, 2, 3])
+    })
+  })
+
+  describe("partial", () => {
+    it("accepts an 'x' arity function and an array of 'n' arguments and returns a new function accepting 'x - n' arguments applying them all to the original function", () => {
+      const subject = K.partial((w, x, y, z) => [w, x, y, z], [0, 1])
+      expect(subject).toBeInstanceOf(Function)
+      expect(subject(2, 3)).toEqual([0, 1, 2, 3])
+    })
+  })
+
+  describe("map", () => {
+    it("applies a function to each item in an array returning a new array from the resulting values", () => {
+      const subject = K.map(x => x * x, [1, 2, 3])
+      expect(subject).toEqual([1, 4, 9])
+    })
+  })
+
+  describe("filter", () => {
+    it("applies a predicate function to each item in an array and returns a new array with the values which pass the predicate", () => {
+      const subject = K.filter(x => x > 3, [1, 2, 3, 4, 5, 6])
+      expect(subject).toEqual([4, 5, 6])
+    })
+  })
+
+  describe("reduce", () => {
+    it("applies a function to an accumulated value and each item in an array, accumulator initial value is specified", () => {
+      const subject = K.reduce((a, x) => a + x, 0, [1, 2, 3])
+      expect(subject).toEqual(6)
+    })
+  })
+
+  describe("pipe", () => {
+    it("threads a value through an array of unary functions from left to right", () => {
+      const subject: number = K.pipe(
+        [(x: number): number => x + 1, (x: number): number => x * 2],
+        1
+      )
+      expect(subject).toEqual(4)
+    })
+  })
+
+  describe("compose", () => {
+    it("threads a value through an array of unary functions from right to left", () => {
+      const subject = K.compose(
+        [x => x + 1, x => x * 2],
+        1
+      )
+      expect(subject).toEqual(3)
+    })
+  })
+
+  describe("get", () => {
+    it("accepts a string key and an object and returns the value at that key", () => {
+      const subject = K.get("key", { key: "VALUE" })
+      expect(subject).toEqual("VALUE")
     })
 
-    describe("pipe", () => {
-        it("accepts an array of functions and a value and threads value through functions in order", () => {
-            const subject: number = pipe([
-                (x: number): number => x + 1,
-                (x: number): number => x * x
-            ], 2)
-            expect(subject).toEqual(9)
-        })
+    it("accepts an integer and an array and returns the value at that index", () => {
+      const subject = K.get(1, [0, "VALUE"])
+      expect(subject).toEqual("VALUE")
+    })
+  })
+
+  describe("getIn", () => {
+    it("accepts an array of string keys and returns the corresponding nested value inside an object", () => {
+      const subject = K.getIn(["x", "y", "z"], { x: { y: { z: "VALUE" } } })
+      expect(subject).toEqual("VALUE")
     })
 
-    describe("compose", () => {
-        it("accepts an array of functions and a value and threads value through functions in reverse order", () => {
-            const subject = compose([
-                x => x + 1,
-                x => x *x
-            ], 2)
-            expect(subject).toEqual(5)
-        })
+    it("accepts an array of integers and returns the corresponding nested value inside an array", () => {
+      const subject = K.getIn([0, 1, 2], [[0, [0, 1, "VALUE"]]])
+      expect(subject).toEqual("VALUE")
     })
+
+    it("accepts an array of string keys and integers and returns the corresponding value inside a complex data structure", () => {
+      const subject = K.getIn(["x", 0, "y", 1], { x: [{ y: [0, "VALUE"] }] })
+      expect(subject).toEqual("VALUE")
+    })
+  })
+
+  describe("assoc", () => {
+    it("accepts a string key, a value, and an object and returns a new object with the objects key associated with the new value", () => {
+      const subject = K.assoc("x", "VALUE", { x: "ORIGINAL" })
+      expect(subject).toEqual({
+        x: "VALUE"
+      })
+    })
+
+    it("accepts an integer, a value, and an array and returns a new array with the index associated with the new value", () => {
+      const subject = K.assoc(1, "VALUE", [0, "ORIGINAL"])
+      expect(subject).toEqual([0, "VALUE"])
+    })
+  })
+
+  describe("assocIn", () => {
+    it("accepts an array of string keys, a value, and an object and returns a new object with the nested value associated with the nested keys", () => {
+      const subject = K.assocIn(["x", "y", "z"], "VALUE", {
+        x: { y: { z: "ORIGINAL" } }
+      })
+      expect(subject).toEqual({
+        x: {
+          y: {
+            z: "VALUE"
+          }
+        }
+      })
+    })
+
+    it("accepts an array of integers, a value, and an array and returns a new array with the nested value asssociated with the nested indices", () => {
+      const subject = K.assocIn([0, 1, 2], "VALUE", [[0, [0, 1, "ORIGINAL"]]])
+      expect(subject).toEqual([[0, [0, 1, "VALUE"]]])
+    })
+
+    it("accepts an array of mixed string keys and integers, a value, and an array and returns a new complex data structure with the value associated with the nested path", () => {
+      const subject = K.assocIn(["x", 0, "y", 1], "VALUE", {
+        x: [{ y: [0, "ORIGINAL"] }]
+      })
+      expect(subject).toEqual({ x: [{ y: [0, "VALUE"] }] })
+    })
+  })
+
+  describe("contains", () => {
+    it("accepts a value and an array and returns true if the value is an element in the array", () => {
+      const subject = K.contains("x", ["x", "y"])
+      expect(subject).toBe(true)
+    })
+  })
+
+  describe("append", () => {
+    it("accepts a value and an array and returns a new array with the value appended to the original", () => {
+      const subject = K.append("VALUE", ["ORIGINAL"])
+      expect(subject).toEqual(["ORIGINAL", "VALUE"])
+    })
+  })
+
+  describe("equal", () => {
+    it("returns true if both arguments are strictly (===) equal", () => {
+      const subject = K.equals("1", "1")
+      expect(subject).toBe(true)
+    })
+  })
+
+  describe("toggleIn", () => {
+    it("appends a value to an array if it is not already present", () => {
+      const subject = K.toggleIn("VALUE", ["ORIGINAL"])
+      expect(subject).toEqual(["ORIGINAL", "VALUE"])
+    })
+
+    it("removes a value from an array if it is already an element", () => {
+      const subject = K.toggleIn("VALUE", ["ORIGINAL", "VALUE"])
+      expect(subject).toEqual(["ORIGINAL"])
+    })
+  })
 })
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,20 +3,83 @@
  */
 type tUnary<A, R> = (A) => R
 type tBinary<A1, A2, R> = (A1, A2) => R
-type tVariant<A extends any[], R> = (...args: A) => R
+type tVariant<R> = (...args: any[]) => R
+type tProp = string | number
+type tPath = ReadonlyArray<tProp>
 
-type tPartial = <A extends any[], R>(fn: tVariant<A, R>, args: any[]) => (...rest: any[]) => R
-export const partial: tPartial = (fn, args) => (...argsRest) => fn(...args, ...argsRest)
+/*
+ * Apply
+ */
+type tApply = <A, R>(fn: (...args: any[]) => R, args: any[]) => R
+export const apply = (fn, args) => fn(...args)
 
+/*
+ * Partial
+ */
+type tPartial = <R>(fn: tVariant<R>, args: any[]) => (...rest: any[]) => R
+export const partial: tPartial = (fn, args) => (...rest) => fn(...args, ...rest)
+
+/*
+ * Pipe
+ */
 type tPipe = {
-    <V, R1>(fns: [tUnary<V, R1>], val: V): R1
-    <V, R1, R2>(fns: [(arg: V) => R1, (arg: R1) => R2], val: V): R2
-    <V, R1, R2, R3>(fns: [(arg: V) => R1, (arg: R1) => R2, (arg: R2) => R3], val: V): R3;
-    <V, R1, R2, R3, R4>(fns: [(arg: V) => R1, (arg: R1) => R2, (arg: R2) => R3, (arg: R3) => R4], val: V): R4;
-    <V, R1, R2, R3, R4, R5>(fns: [(arg: V) => R1, (arg: R1) => R2, (arg: R2) => R3, (arg: R3) => R4, (arg: R4) => R5], val: V): R5;
+  <V, R1>(fns: [tUnary<V, R1>], val: V): R1
+  <V, R1, R2>(fns: [(arg: V) => R1, (arg: R1) => R2], val: V): R2
+  <V, R1, R2, R3>(
+    fns: [(arg: V) => R1, (arg: R1) => R2, (arg: R2) => R3],
+    val: V
+  ): R3
+  <V, R1, R2, R3, R4>(
+    fns: [(arg: V) => R1, (arg: R1) => R2, (arg: R2) => R3, (arg: R3) => R4],
+    val: V
+  ): R4
+  <V, R1, R2, R3, R4, R5>(
+    fns: [
+      (arg: V) => R1,
+      (arg: R1) => R2,
+      (arg: R2) => R3,
+      (arg: R3) => R4,
+      (arg: R4) => R5
+    ],
+    val: V
+  ): R5
 }
 // type tPipe = <V, R>(fns: Array<tUnary<V, R>>, val: V) => R
-export const pipe: tPipe = (fns, val) => fns.reduce((accum, fn) => fn(accum), val)
-const x: number = pipe([(y: number): number => y + 1, (y: number): number => y + 1, (y: number): number => y + 1], 1);
+export const pipe: tPipe = (fns, val) =>
+  fns.reduce((accum, fn) => fn(accum), val)
 
-export const compose = (fns, val) => fns.reduceRight((accum, fn) => fn(accum), val)
+/*
+ * Compose
+ */
+export const compose = (fns, val) =>
+  fns.reduceRight((accum, fn) => fn(accum), val)
+
+export const map = (fn, arr) => arr.map(fn)
+export const filter = (fn, arr) => arr.filter(fn)
+export const reduce = (fn, initialVal, arr) => arr.reduce(fn, initialVal)
+
+export const get = (key, val) => val[key]
+export const getIn = (keys, val) =>
+  reduce((accum, key) => get(key, accum), val, keys)
+
+const assocArr = (key, val, arr) => Object.assign([...arr], { [key]: val })
+const assocObj = (key, val, obj) => Object.assign({ ...obj }, { [key]: val })
+export const assoc = (key, val, data) =>
+  apply(Array.isArray(data) ? assocArr : assocObj, [key, val, data])
+
+type tAssocIn = <V, O>(path: tPath, val: V, obj: O) => O
+export const assocIn: tAssocIn = ([key, ...rest], val, obj) =>
+  assoc(key, rest.length ? assocIn(rest, val, obj[key]) : val, obj)
+
+export const contains = (val, arr) => arr.includes(val)
+export const append = (val, arr) => [...arr, val]
+export const concat = (arr, val) => [...arr, ...val]
+export const remove = (val, arr) => filter(item => !equals(item, val), arr)
+export const equals = (x, y) => x === y
+export const toggleIn = (val, arr) =>
+  contains(val, arr) ? remove(val, arr) : append(val, arr)
+
+export const add = (x, y) => x + y
+
+export const merge = (x, y) => ({ ...x, ...y })
+export const reverse = x => x.reverse()
